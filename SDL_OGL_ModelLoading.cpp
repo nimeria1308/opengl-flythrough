f59be7660156e20c971126837b93830f0b527170
Simona Dimitrova <nimeria1308@gmail.com>
2020-04-21 22:10:27 +0300

    downgraded to OGL 3.1 or it would crash

--- SDL_OGL_ModelLoading.cpp
+++ SDL_OGL_ModelLoading.cpp
@@ -167,7 +167,7 @@ bool init()
 		//Use OpenGL 3.3
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
 
 		//Create window
